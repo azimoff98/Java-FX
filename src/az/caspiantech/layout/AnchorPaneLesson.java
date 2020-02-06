67ac616add8eff1329d5d25b83e225b313286c21
azimovgrbn <azimoff.q@gmail.com>
2020-02-06 18:05:27 +0400

    ui_controls

--- src/az/caspiantech/layout/AnchorPaneLesson.java
+++ src/az/caspiantech/layout/AnchorPaneLesson.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -20,6 +21,7 @@ public class AnchorPaneLesson extends Application {
     public void start(Stage primaryStage) throws Exception {
         Group root = new Group();
 
+        ChoiceBox<String> choiceBox = new ChoiceBox<>();
         AnchorPane anchorPane = new AnchorPane();
         AnchorPane.setBottomAnchor(new TextArea("TEXT"), 10.0);
         anchorPane.getChildren().addAll(new Button("Start"));
